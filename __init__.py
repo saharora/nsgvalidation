9d8d783643add64062a78a2eae0970c5bb2bc77c
root <root@haxv-niarora-1.ciena.com>
2020-04-22 02:21:51 +0530

    nsg

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from nsgvalidation import validate_sg
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='nsgvalidation',  
+     version='0.2',
+     scripts=['nsgvalidation'] ,
+     author="Sahil Arora",
+     author_email="saharora@ciena.com",
+     description="A Utility package for Azure NSG validation",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/saharora/nsgvalidation",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     python_requires='>=3.6',
+ )
